293da5106bfd63081f7a18c3312d0e64a0167e07
Herman Õunapuu <hermanounapuu@gmail.com>
2018-04-28 18:46:43 +0300

    redid tests

---
+++ tests/performance-testing-unigine.sh
@@ -0,0 +1,24 @@
+echo "powersave mode test"
+python3 ../client powersave
+
+#run tests with monitoring all data
+MONITOR=all phoronix-test-suite benchmark pts/unigine-sanctuary
+
+sleep 60
+
+echo "normal mode test"
+python3 ../client normal
+
+#run tests with monitoring all data
+MONITOR=all phoronix-test-suite benchmark pts/unigine-sanctuary
+
+sleep 60
+
+echo "performance mode test"
+python3 ../client performance
+
+#run tests with monitoring all data
+MONITOR=all phoronix-test-suite benchmark pts/unigine-sanctuary
+
+sleep 60
+
